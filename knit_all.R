32fcccf95d64766c3848f71bd2d39a640926585d
Lauren Chenarides <Lauren.Chenarides@colostate.edu>
2024-07-15 14:13:52 -0600

    had to knit the student pages

--- knit_all.R
+++ knit_all.R
@@ -1,8 +1,24 @@
+#==== SPEAKERS ====#
 # Load necessary library
 library(rmarkdown)
 
 # Set the working directory to the folder containing the Rmd files
 setwd("C:/Users/lachenar/OneDrive - Colostate/Documents/GitProjectsWithR/usda_food_prices_workshop/speakers")
+
+# List all Rmd files in the directory
+rmd_files <- list.files(pattern = "\\.Rmd$")
+
+# Loop through the files and render them
+for(file in rmd_files) {
+  render(file)
+}
+
+#==== STUDENTS ====#
+# Load necessary library
+library(rmarkdown)
+
+# Set the working directory to the folder containing the Rmd files
+setwd("C:/Users/lachenar/OneDrive - Colostate/Documents/GitProjectsWithR/usda_food_prices_workshop/students")
 
 # List all Rmd files in the directory
 rmd_files <- list.files(pattern = "\\.Rmd$")
